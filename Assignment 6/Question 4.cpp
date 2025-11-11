6cc3e2d18f136781c40280f03e895f139e86b359
bhaveshkanjwani-max <bhaveshkanjwani@gmail.com>
2025-11-11 11:58:47 +0530

    Create Question 4.cpp

---
+++ Assignment 6/Question 4.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std ;
+class person
+{
+	protected:
+	int age;
+	string name;
+};
+class student :public person
+{
+	protected:
+		int sports,academics;
+		
+};
+class result:public person
+{
+	protected:
+		
+};
+class sports
+{
+	
+};
+class academics
+{
+	
+};
+int main()
+{
+	
+	return 0 ;
+}
+
